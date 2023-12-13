294b6f39d4bbf513bc3bae8db057c4a75d8d3bd5
Md. Asaduzzman Nur Shuvo <nur.shuvo@samsung.com>
2023-12-13 17:27:36 +0600

    Partial home screen

--- app/src/main/java/com/developerspace/webrtcsample/compose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/developerspace/webrtcsample/compose/ui/screens/HomeScreen.kt
@@ -7,14 +7,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -24,25 +22,37 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
 import coil.transform.RoundedCornersTransformation
 import com.developerspace.webrtcsample.compose.ui.theming.MyTheme
 import com.developerspace.webrtcsample.compose.ui.theming.lightGreen
 import com.developerspace.webrtcsample.model.User
-import org.intellij.lang.annotations.JdkConstants.FontStyle
-import java.time.format.TextStyle
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(24.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
         ListOfStory()
+        Text(
+            "Top topics that you want to discuss",
+            modifier = Modifier.padding(5.dp),
+            style = TextStyle(
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily.Serif,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        ListOfTopCategoryCards()
     }
 }
 
@@ -87,8 +97,43 @@ fun StoryCard(onClickCard: () -> Unit) {
                     .align(Alignment.BottomCenter)
                     .padding(bottom = 5.dp)
             )
-            ProfilePicture(user = User(), size = 20.dp, modifier = Modifier.align(Alignment.TopStart))
+            ProfilePicture(
+                user = User(),
+                size = 20.dp,
+                modifier = Modifier.align(Alignment.TopStart)
+            )
         }
+    }
+}
+
+@Composable
+fun ListOfTopCategoryCards() {
+    LazyColumn {
+        items(6) {
+            CategoryCardTuple()
+        }
+    }
+}
+
+@Composable
+fun CategoryCardTuple() {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+        TopicCard()
+        TopicCard()
+        TopicCard()
+    }
+}
+
+@Composable
+fun TopicCard() {
+    Card(
+        shape = RoundedCornerShape(15.dp),
+        modifier = Modifier
+            .width(100.dp)
+            .height(140.dp)
+            .padding(2.dp)
+            .border(2.dp, lightGreen, RoundedCornerShape(15.dp))
+    ) {
     }
 }
 
