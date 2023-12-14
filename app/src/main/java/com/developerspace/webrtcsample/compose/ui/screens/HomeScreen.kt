ece833faa317b1a4366408b3e2dbc2ecb69f8c59
Md. Asaduzzman Nur Shuvo <nur.shuvo@samsung.com>
2023-12-14 17:08:20 +0600

    Home Screen partial - 03

--- app/src/main/java/com/developerspace/webrtcsample/compose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/developerspace/webrtcsample/compose/ui/screens/HomeScreen.kt
@@ -11,15 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -27,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -36,8 +34,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
 import coil.transform.RoundedCornersTransformation
+import com.developerspace.webrtcsample.R
 import com.developerspace.webrtcsample.compose.ui.theming.MyTheme
 import com.developerspace.webrtcsample.compose.ui.theming.lightGreen
 import com.developerspace.webrtcsample.compose.ui.viewmodel.HomeScreenViewModel
@@ -139,7 +137,7 @@ fun CategoryCardTuple(cnt: Int) {
 }
 
 @Composable
-fun TopicCard(cnt: Int, color: Color = lightGreen) {
+fun TopicCard(cnt: Int, color: Color = Color.White) {
     Card(
         shape = RoundedCornerShape(15.dp),
         modifier = Modifier
@@ -147,18 +145,23 @@ fun TopicCard(cnt: Int, color: Color = lightGreen) {
             .height(140.dp)
             .padding(2.dp)
             .border(2.dp, lightGreen, RoundedCornerShape(15.dp)),
-         colors = CardDefaults.cardColors(
+        colors = CardDefaults.cardColors(
             containerColor = color,
         )
     ) {
         if (cnt % 2 == 0) {
             Image(
-                painter = rememberImagePainter(
-                    data = "https://images.unsplash.com/photo-1485290334039-a3c69043e517?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-                ),
-                modifier = Modifier.width(100.dp).height(140.dp),
+                painterResource(id = R.drawable.childhood),
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(140.dp),
                 contentScale = ContentScale.Crop,
                 contentDescription = ""
+            )
+        } else {
+            Text(modifier = Modifier.fillMaxSize(),
+                text = "Childhood",
+                fontSize = 20.sp
             )
         }
     }
