f080f7a0accd48a1c7d7a42a8248b31ccc195888
Nur Shuvo <nurshuvo51@gmail.com>
2023-12-29 12:48:10 +0600

    Slight refactoring

--- app/src/main/java/com/developerspace/webrtcsample/compose/ui/util/AppLevelCache.kt
+++ app/src/main/java/com/developerspace/webrtcsample/compose/ui/util/AppLevelCache.kt
@@ -2,7 +2,8 @@ package com.developerspace.webrtcsample.compose.ui.util
 
 import com.developerspace.webrtcsample.model.User
 
+// TODO Remove this class, instead rewrite logic so that userDB is sufficient.
 object AppLevelCache {
     var userProfiles: MutableList<User>?= null
-    var currentUserItemKey: Int = 0
+    var currentUserItemKey: Int = 0 // inedx of current user in cache list userProfiles
 }
